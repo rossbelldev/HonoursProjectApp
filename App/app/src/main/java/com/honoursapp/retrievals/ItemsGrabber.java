a5f84aa3e174cabf077f1ea4acd38940b11a5dd5
theRGTgamer <40317736@live.napier.ac.uk>
2021-01-01 18:37:14 +0000

    now joined with firebase

--- App/app/src/main/java/com/honoursapp/retrievals/ItemsGrabber.java
+++ App/app/src/main/java/com/honoursapp/retrievals/ItemsGrabber.java
@@ -1,4 +1,15 @@
 package com.honoursapp.retrievals;
 
+import android.util.Log;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 class ItemsGrabber {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+
+
+
 }
