58314d128f10f6e41f0abe842bb4918aaa86f47a
theRGTgamer <rosscameronclarkbell@gmail.com>
2021-01-04 02:06:37 +0000

    Start of item display

--- App/app/src/main/java/com/honoursapp/activities/ViewItemTemplate.java
+++ App/app/src/main/java/com/honoursapp/activities/ViewItemTemplate.java
@@ -12,5 +12,15 @@ public class ViewItemTemplate extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_item_template);
+
+
+        //The start of item display!
+
+        //Get the passed object
+
+        //Send it off to be swapped to a more usable format
+        
+        //React to the information which it returns
+
     }
 }
--- App/app/src/main/java/com/honoursapp/calculations/SwapIDBtoItem.java
+++ App/app/src/main/java/com/honoursapp/calculations/SwapIDBtoItem.java
@@ -1,4 +1,5 @@
 package com.honoursapp.calculations;
 
 public class SwapIDBtoItem {
+
 }
