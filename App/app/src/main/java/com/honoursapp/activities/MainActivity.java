49c416810755304c98055f61d8f2d573770df065
theRGTgamer <rosscameronclarkbell@gmail.com>
2021-01-13 02:22:13 +0000

    Can check if user is signed in or not, they sign in automatically. Code for this check has been left in and will be relied upon at a later date

--- App/app/src/main/java/com/honoursapp/activities/MainActivity.java
+++ App/app/src/main/java/com/honoursapp/activities/MainActivity.java
@@ -6,13 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.honoursapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
     //Create buttons to be used across program
-    Button btnBurger, btnOrderToTable, btnOrderForCollection, btnBookTable;
+    Button btnBurger, btnOrderToTable, btnOrderForCollection, btnBookTable, btnCheck;
+
+    //Firebase
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
         btnOrderToTable = (Button) findViewById(R.id.btnOrderToTable);
         btnOrderForCollection = (Button) findViewById(R.id.btnOrderForCollection);
         btnBookTable = (Button) findViewById(R.id.btnBookTable);
+        btnCheck = (Button) findViewById(R.id.btnCheckSignIn);
+
+        //Initialise the firebase authentication
+        auth = FirebaseAuth.getInstance();
 
         //On click listeners for buttons
         btnOrderToTable.setOnClickListener(new View.OnClickListener() {
@@ -63,6 +73,23 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        //Can test if a user is signed in or not. If they are then proceed, else get them to register. (Will be done automatically at some point)
+        /*
+        btnCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Check to see if the user is signed in
+                FirebaseUser u = auth.getCurrentUser();
+                if(u != null){
+                    String s = "The user is currently signed in! " + u.getEmail();
+                    Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "No user is currently signed in", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+         */
 
     }
 }
